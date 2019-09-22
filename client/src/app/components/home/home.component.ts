cf12691fb6311740621a0e677e7ed4dedff9ad41
vinson aiono <vinson.aiono@gmail.com>
2019-09-21 21:12:09 -0700

    initial commit

---
+++ client/src/app/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+declare let alertify:any;
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  runSuccess(){
+    alertify.success("Vinson is smart")
+  }
+}
